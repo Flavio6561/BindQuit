b8ba67354d9a0442f4083f20ec1566d0f0a8176e
flavio6561 <flaviosessantacinquesessantuno@gmail.com>
2024-10-06 12:16:34 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+## Bindquit,
+a simple client side mod that disconnects you with the press of a button
+
+### Config:
+> - Keybind changes will be saved automatically
+
+### Other:
+
+> The mod is entirely client side, this will make the mod work on servers, but will not affect server interactions
+
+### Links:
+> - [GitHub page](https://github.com/Flavio6561/BindQuit)  
+> - [Modrinth page (soon)](https://modrinth.com/mod/BindQuit)  
+> - [Changelogs](https://github.com/Flavio6561/BindQuit/wiki/Version-changelogs)  
+> - [Report an issue](https://github.com/Flavio6561/BindQuit/issues)
---
+++ src/client/java/com/bindquit/BindQuitClient.java
@@ -0,0 +1,35 @@
+package com.bindquit;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.gui.screen.DisconnectedScreen;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.text.Text;
+import org.lwjgl.glfw.GLFW;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class BindQuitClient implements ClientModInitializer {
+	private static final Logger LOGGER = LoggerFactory.getLogger(BindQuitClient.class);
+	private static KeyBinding disconnectKey;
+
+	@Override
+	public void onInitializeClient() {
+		LOGGER.info("BindQuit initialized with success");
+		disconnectKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+				"Quit bind",
+				GLFW.GLFW_KEY_K,
+				"BindQuit"
+		));
+
+		ClientTickEvents.END_CLIENT_TICK.register(client -> {
+			if (disconnectKey.wasPressed()) {
+				if (client.world != null && !client.isInSingleplayer()) {
+					client.disconnect();
+					client.setScreen(new DisconnectedScreen(null, Text.literal(""), Text.literal("You successfully disconnected")));
+				}
+			}
+		});
+	}
+}
