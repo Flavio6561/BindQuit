d0797a99bf1cd298e83f05dc70b5bc24efc24599
flavio6561 <flaviosessantacinquesessantuno@gmail.com>
2025-01-17 15:54:58 +0100

    Version 1.1.2 Changelogs: https://github.com/Flavio6561/BindQuit/wiki/Version-changelogs

--- src/client/java/com/bindquit/BindQuitClient.java
+++ src/client/java/com/bindquit/BindQuitClient.java
@@ -5,7 +5,6 @@ import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.minecraft.client.gui.screen.DisconnectedScreen;
 import net.minecraft.client.option.KeyBinding;
-import net.minecraft.server.MinecraftServer;
 import net.minecraft.text.Text;
 import org.lwjgl.glfw.GLFW;
 import org.slf4j.Logger;
@@ -19,20 +18,20 @@ public class BindQuitClient implements ClientModInitializer {
 	public void onInitializeClient() {
 		LOGGER.info("BindQuit initialized with success");
 		disconnectKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
-				"Quit bind",
+				"Disconnect key",
 				GLFW.GLFW_KEY_K,
 				"BindQuit"
 		));
-
 		ClientTickEvents.END_CLIENT_TICK.register(client -> {
 			if (disconnectKey.wasPressed()) {
-				if (client.world != null && !client.isInSingleplayer()) {
-					client.disconnect(new DisconnectedScreen(null, Text.literal(""), Text.literal("You successfully disconnected")));
-				} else if (client.isInSingleplayer()) {
-					MinecraftServer server = client.getServer();
-					if (server != null) {
-						server.stop(true);
-						client.disconnect(new DisconnectedScreen(null, Text.literal(""), Text.literal("You successfully saved and quit")));
+				if (client.isInSingleplayer()) {
+					if (client.getServer() != null) {
+						client.getServer().stop(true);
+						client.disconnect(new DisconnectedScreen(null, Text.literal(""), Text.literal("You saved and quit")));
+					}
+				} else {
+					if (client.getNetworkHandler() != null && client.getNetworkHandler().getConnection() != null) {
+						client.getNetworkHandler().getConnection().disconnect(Text.literal("You disconnected"));
 					}
 				}
 			}
